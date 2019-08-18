3332d030b14cc8565162e6a961dfcce5e4cf3470
Diego Gomez <berna9803@gmail.com>
2019-08-17 20:14:51 -0500

    migrations

---
+++ database/migrations/2019_08_17_173657_create_categorias_productos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriasProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorias_productos', function (Blueprint $table) {
+            $table->bigIncrements('id_categoria_producto');
+            $table->string('nombre')->nullable();
+           
+
+
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categorias_productos');
+    }
+}
---
+++ database/migrations/2019_08_17_174934_create_presentaciones_productos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePresentacionesProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('presentaciones_productos', function (Blueprint $table) {
+            $table->bigIncrements('id_presentacion_producto');
+            $table->string('descripcion')->nullable();
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('presentaciones_productos');
+    }
+}

fd518f9aad3d3935e56cd9adadfe0e2102499b4f
Dan <dannarubio2198@gmail.com>
2019-08-18 09:26:54 -0500

    1.0

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,4 @@
+
+
+<h1>No se ha encontrado el recurso solicitado</h1>
+<a href="/">Volver a la página inicial</a>
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,24 @@
+<div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Menu
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+      <button class="dropdown-item" type="button">Clientes</button>
+      <button class="dropdown-item" type="button">Proveedores</button>
+      <button class="dropdown-item" type="button">Personal</button>
+      <button class="dropdown-item" type="button">Catalogo Producto</button>
+      <button class="dropdown-item" type="button">Presentaciones Producto</button>
+      <button class="dropdown-item" type="button">Productos</button>
+      <button class="dropdown-item" type="button">Pagos clientes</button>
+      <button class="dropdown-item" type="button">Pagos proveedores</button>
+      <button class="dropdown-item" type="button">Ventas</button>
+      <button class="dropdown-item" type="button">Bajas ventas</button>
+      <button class="dropdown-item" type="button">Detalles ventas</button>
+      <button class="dropdown-item" type="button">Devoluciones ventas</button>
+      <button class="dropdown-item" type="button">Detalles devoluciones ventas</button>
+      <button class="dropdown-item" type="button">Compras</button>
+      <button class="dropdown-item" type="button">Detalles compras</button>
+      <button class="dropdown-item" type="button">Devoluciones compras</button>
+      <button class="dropdown-item" type="button">Detalles devoluciones compras</button>
+    </div>
+  </div>
